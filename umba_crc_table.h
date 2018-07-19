f627c77a9c1bdb69ad82a53acbf0b035d52cf5b6
ArtemZaZ <temka.911@mail.ru>
2018-07-19 13:13:31 +0300

    Squashed 'src/crc/' content from commit 4793d47
    git-subtree-dir: src/crc
    git-subtree-split: 4793d47118dbd8c031834706f7013f7117b3a63d

---
+++ umba_crc_table.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+uint8_t umba_crc8_table (const uint8_t * buf, uint32_t len);
+uint16_t umba_crc16_table (const uint8_t * buf, uint32_t len);
+uint32_t umba_crc32_table (const uint8_t * buf, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
